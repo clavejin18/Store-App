5366435c54c838978a36f6c698b05edbd26306c5
Jaime Perez <clavejin18@gmail.com>
2019-05-13 21:35:55 -0700

    schema file and cvs file finished

---
+++ bamazon.sql
@@ -0,0 +1,20 @@
+-- Dropping database if it exists 
+DROP DATABASE IF EXISTS amazonDB;
+
+-- Create database if it doesn't exist
+CREATE database amazonDB;
+
+-- Setting database
+USE amazonDB;
+
+-- Creating table 
+CREATE TABLE products (
+    sku INT NOT NULL,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(100) NULL,
+    price INT NOT NULL,
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY(sku)
+);
+-- ingesting file
+SELECT * FROM amazonDB.products;
